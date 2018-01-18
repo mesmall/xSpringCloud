e189fe27c99630a5030d01160a02bc7d58dbabc8
lizhen <2225213990@qq.com>
2018-01-18 17:45:15 +0800

    1、eurekaServer 服务注册中心 可以为高可用集群分布 2、eurekaClient 服务提供者   可以为高可用集群分布 3、eurekaConsumerRibbonHystrix 服务消费者，熔断，负载均衡机制处理 4、hystrixDashboard 服务调用监控信息

---
+++ eurekaServer/src/main/java/com/base/constants/ConstantsProperties.java
@@ -0,0 +1,26 @@
+package com.base.constants;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by root on 2017/9/26.下午5:02
+ *
+ * @Author : lz
+ * @Description:
+ */
+@Component
+public class ConstantsProperties {
+
+    @Value("${spring.profiles.active}")
+    private String dev;
+
+
+    public String getDev() {
+        return dev;
+    }
+
+    public void setDev(String dev) {
+        this.dev = dev;
+    }
+}
